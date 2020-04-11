e86e3fdaec1cc1a079f8e2f64473e056a0c18dfc
Paul Jimenez <paul@rocketlabs.mx>
2020-04-11 16:06:36 -0700

    Responsive for mobile devices

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,25 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
   // Sidenav's initial state
-  open: boolean = true;  
-  constructor() { }
+  open: boolean;
+
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+
+  fillerContent = Array.from({ length: 50 }, () =>
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(min-width: 768px)');
+    this.open = this.mobileQuery.matches;
+    console.log(this.open);
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
   ngOnInit() {
   }
-  onOpen(){
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  onOpen() {
     // console.log('Opened');
   }
-  onClose(){
+  onClose() {
     // console.log('Closed');
   }
   // app-menu output event handler
-  onSideMenuToggle(event){
-    this.open = event;
+  onSideMenuToggle(event) {
+    this.open = !this.open;
   }
 }

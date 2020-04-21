795f17c413fe91b9718bc8f02382abf0755fbda5
Paul Jimenez <paul@rocketlabs.mx>
2020-04-20 19:01:16 -0700

    folder rename

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
-import { SideMenuModule } from './componentModules/side-menu/side-menu.module';
-import { MenuModule } from './componentModules/menu/menu.module';
+import { SideMenuModule } from './dashboard-components/side-menu/side-menu.module';
+import { MenuModule } from './dashboard-components/menu/menu.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { ImageFavoritesModule } from '../pageModules/image-favorites/image-favorites.module';
 import { ImageViewerModule } from '../pageModules/image-viewer/image-viewer.module';

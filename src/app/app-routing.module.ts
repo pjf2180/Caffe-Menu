92f354e78ce42c4f7166e0ad5a66ab9ef6097e88
Paul Jimenez <pjf2180@me.com>
2020-06-09 12:26:07 -0700

    Fixed app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'signin',loadChildren: () => import('./pageModules/signin-signup/signin-signup.module').then(m => m.SigninSignupModule)  },
+  
   { path: '',loadChildren: () => import('./pageModules/user/user.module').then(m => m.UserModule)  },
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
 ];

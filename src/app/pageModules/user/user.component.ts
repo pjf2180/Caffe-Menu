4689febafcb69d7999f6907511df06ea26769747
Paul Jimenez <pjf2180@me.com>
2020-06-09 13:25:00 -0700

    Sign in and register pages made responsive

--- src/app/pageModules/user/user.component.ts
+++ src/app/pageModules/user/user.component.ts
@@ -17,7 +17,6 @@ export class UserComponent implements OnInit, OnDestroy {
 
   constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(min-width: 768px)');
-    this.open = this.mobileQuery.matches;
     this.matSideNavMode = this.mobileQuery.matches ? 'side' : 'over';
     console.log(this.open);
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();

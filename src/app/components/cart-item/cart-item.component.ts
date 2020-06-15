4320551503dee6ff53ff1edd054ca358addb96ef
Paul Jimenez <pjf2180@me.com>
2020-06-15 12:02:07 -0700

    Cart item dynamic content added

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CartItem } from 'src/app/models/cart-item.models';
 
 @Component({
   selector: 'app-cart-item',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartItemComponent implements OnInit {
 
+  @Input() item: CartItem;
+  
   constructor() { }
 
   ngOnInit() {

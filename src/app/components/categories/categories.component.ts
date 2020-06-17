65bdc20ab6ceae6770c71b0be4d886c157886b52
Paul Jimenez <pjf2180@me.com>
2020-06-16 21:48:04 -0700

    Added menu selectors

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ShoppingProductService } from 'src/app/services/product.service';
 import { ProductCategory } from 'src/app/models/product-category.models';
 import { Observable } from 'rxjs';
-import { ShoppingProduct } from 'src/app/models/shopping-product';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/root-reducer';
-import { selectMenuItems, selectMenuCategories } from 'src/app/store/menu/selectors/menu.selectors';
+import { selectMenuWithCriteria } from 'src/app/store/menu/selectors/menu.selectors';
 import * as fromMenuActions from '../../store/menu/actions/menu.actions'
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-categories',
@@ -18,13 +15,11 @@ import { map } from 'rxjs/operators';
 export class CategoriesComponent implements OnInit {
 
   categories: Observable<ProductCategory[]>;
-  products: Observable<ShoppingProduct[]>;
 
   constructor(public store: Store<AppState>) { }
 
   ngOnInit() {
-    this.products = this.store.select(selectMenuItems);
-    this.categories = this.store.select(selectMenuCategories);
+    this.categories = this.store.select(selectMenuWithCriteria, { criteria: null, all: true });
     this.store.dispatch(fromMenuActions.loadMenus());
   }
 
--- src/app/store/menu/selectors/menu.selectors.ts
+++ src/app/store/menu/selectors/menu.selectors.ts
@@ -1,6 +1,8 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromMenu from '../reducers/menu.reducer';
 import { groupProductsByCategory } from '../utils.menu'
+import { ShoppingProduct } from 'src/app/models/shopping-product';
+import { authFeatureKey } from '../../auth/reducers/auth.reducer';
 
 export const selectMenuState = createFeatureSelector<fromMenu.State>(
   fromMenu.menuFeatureKey
@@ -11,7 +13,23 @@ export const selectMenuItems = createSelector(
   menuState => menuState.entities
 );
 
-export const selectMenuCategories = createSelector(
+export const selectMenu = createSelector(
   selectMenuItems,
+  (menuItems: ShoppingProduct[]) => groupProductsByCategory(menuItems)
+);
+
+export const selectItemsWithCriteria = createSelector(
+  selectMenuItems,
+  (menuItems: ShoppingProduct[], props: { criteria: string, all?: boolean }) => {
+    if (props.all) {
+      return menuItems;
+    } else {
+      menuItems.filter(i => i.name.includes(props.criteria))
+    }
+  }
+)
+
+export const selectMenuWithCriteria = createSelector(
+  selectItemsWithCriteria,
   menuItems => groupProductsByCategory(menuItems)
 );
